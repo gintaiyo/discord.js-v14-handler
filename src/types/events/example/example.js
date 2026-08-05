5a63490788e62524e7744ecab1c7945ffc92e155
銀 Taiyo <95185838+gintaiyo@users.noreply.github.com>
2026-08-05 23:52:12 +0530

    Refactor example.js and add event guide link
    Updated event module with a link to the event guide.

--- src/types/events/example/example.js
+++ src/types/events/example/example.js
@@ -1,8 +1,8 @@
 const { Client } = require('discord.js'); //install things you might required here
 
 module.exports = {
-    name: "name", //it must be name of an event look at event guide on github
-    rest: false,
+    name: "name", //it must be name of an event look at event guide on github https://gist.github.com/Iliannnn/f4985563833e2538b1b96a8cb89d72bb
+    rest: false, 
     once: false,
     /**
      * @param { Client } client
